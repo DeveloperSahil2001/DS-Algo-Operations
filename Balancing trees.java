41b3923a4e4a1f67b73fddc539adea5f4cd2f596
Sahil Goyal <68017381+DeveloperSahil2001@users.noreply.github.com>
2020-10-13 20:16:49 +0530

    Create Balancing trees.java

---
+++ Balancing trees.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+// Other imports go here
+// Do NOT change the class name
+class Main
+{
+    public static void main(String[] args)
+    {
+        // Write your code here
+    }
+    }
