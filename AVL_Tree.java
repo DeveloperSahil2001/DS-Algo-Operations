f9a9acbbc36d1666755faec7d63a2160d3a07f1e
Sahil Goyal <68017381+DeveloperSahil2001@users.noreply.github.com>
2020-10-17 19:49:13 +0530

    Update AVL_Tree.java

--- AVL_Tree.java
+++ AVL_Tree.java
@@ -2,39 +2,70 @@ import java.util.Scanner;
 // Other imports go here
 // Do NOT change the class name
 class Node{
- int data;
+ int data,height;
  Node left,right;
  Node(int data)
  {
   this.data=data;
-  right=left=null;
+  height=1;
  }
 }
-class BalancedTree{
- Node root;
- BalancedTree{
-    this.root=null;
- }
- Node BinarySearch(Node root,int data)
- {
-  if(root==null)
-   return null;
-  else if(root.data==data)
-   return root;
-  else if(root.data<data)
-  {
-   BinarySearch(root.right,data);
+
+class AVL_tree{
+    Node root;
+    int height(Node node)
+    {
+     if(node==null)
+      return 0;
+     return node.height;
+    }
+
+    int max(int a,int b)
+    {
+     return (a>b)?a:b;
+    }
+
+    Node rotate_right(Node node)
+    {
+     Node x=node.left;
+     Node temp=x.right;
+     x.right=node;
+     node.left=temp;
+     node.height=max(node.left,node.right)+1;
+     x.height=max(x.left,x.right)+1;
+     return x;
+    }
+
+    Node rotate_left(Node node)
+    {
+     Node x=node.right;
+     Node temp=x.left;
+     x.left=node;
+     node.right=temp;
+     node.height=max(node.left,node.right)+1;
+     x.height=max(x.left,x.right)+1;
+     return x;
+    }
+
+    int getBalanceFactor(Node node) {
+    if (node==null)
+      return 0;
+    return height(node.left)-height(node.right);
+   }
+
+   void preOrder(Node node) {
+    if (node != null) {
+      System.out.print(node.data + " ");
+      preOrder(node.left);
+      preOrder(node.right);
+    }
   }
-  else
-  {
-   BinarySearch(root.left,data);
-  }
- }
 }
+
 class Main
 {
     public static void main(String[] args)
     {
-        // Write your code here
+     
     }
-    }
+}
